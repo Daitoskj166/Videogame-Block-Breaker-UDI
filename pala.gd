1ebccddb16e68aaa0431a87a45719ff3a99db93c
Daitoskj166 <Daitoskj16@gmail.com>
2026-05-10 22:55:34 -0500

    Ahora sí: Subida real de Element Breaker

---
+++ pala.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+
+func _physics_process(delta):
+	# Capturamos la entrada del teclado (Flechas o A/D)
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Esta función mueve la pala físicamente
+	move_and_slide()
